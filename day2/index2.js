a3d6a655d610a5b8b738498afacd84d6bb6537cc
rajbaghelrahul <rajbaghelrahul@gmail.com>
2022-12-05 18:59:08 +0530

    Closures

--- day2/index2.js
+++ day2/index2.js
@@ -9,14 +9,14 @@
 
 var arr = [1,2,3,4,5]
 
-// for(var i=0; i<arr.length; i++){
+for(var i=0; i<arr.length; i++){
 
-//   setTimeout(()=>console.log(arr[i],i),i*1000);
+  setTimeout(()=>console.log(arr[i],i),i*1000);
 
-// //   setTimeout(() => 
-// //     console.log(arr[i],i),i*1000
-// //   );
-// }
+//   setTimeout(() =>
+//     console.log(arr[i],i),i*1000
+//   );
+}
 
 // console.log('value of i after for loop',i);
 
---
+++ day4/closures/index.js
@@ -0,0 +1,13 @@
+// console.log("Hello world!");
+
+let num = 10;
+function Hello() {
+    function a() {
+        console.log(num);
+    }
+    // num = 20;
+    a();
+}
+Hello();
+// let init = Hello();
+// init();
