28d66d1f09823a12f05ea18291b25268dd3cb5ca
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-01-02 22:03:42 +0530

    day12

---
+++ day12/useRef/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import UseRefComp from "./Components/UseRefComp";
+
+
+function App() {
+  return (
+    <div className="App">
+      <UseRefComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ day12/useRef/src/Components/UseRefComp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function UseRefComp() {
+  return (
+    <>
+      <h1>UseRefComp</h1>
+      <input type="text" placeholder="type some text here...." />
+      <h2>Counter for the rendering: {}</h2>
+    </>
+  );
+}
