c0851fe7a7a097ec1f40ddae4ecdc1bfa7114d87
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-08 11:38:10 +0530

    Interview Prepration

--- day22/InterviewPreparation/index.js
+++ day22/InterviewPreparation/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+// console.log("Hello world!");
+
+console.log(a); // undefined
+var a = 45;
+console.log(a); // 45
+
+
+greet();
+greet1(); // shows Error
+
+function greet() {
+    console.log("Hello world");
+}
+
+const greet1 = () => {
+    console.log("Hello world");
+}
