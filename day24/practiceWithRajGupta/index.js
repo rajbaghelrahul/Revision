5d0f2b30268ad94468377c6465cd02299ce69d64
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-09 20:09:19 +0530

    Practice with Raj

---
+++ day24/practiceWithRajGupta/index.js
@@ -0,0 +1,69 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// console.log("Welcome to Programiz!");
+// let text = ['a','b']
+// text[1] = 'z'
+// console.log(text)
+// string we can't change
+
+// const arr = [1,2,3]
+// arr.length = 0
+// console.log(arr) // []
+
+// const arr = [1,2,3,4]
+// arr[100] = undefined
+// console.log(arr, arr.length)
+// console.log(arr[26], arr.length)
+
+// var arr = new Array(4);
+// console.log(arr[4], arr.length);
+
+// var variable = 10;
+// (() => {
+// 	variable2 = 100;
+// 	console.log(variable); // undefined --> 10
+// 	console.log(variable2); // 100
+// 	variable = 20;
+// 	var variable2 = 50;
+// 	console.log(variable); // 20
+// })();
+// console.log(variable); //20
+// var variable = 30
+// console.log(variable2); // 50
+
+
+
+
+// function raj(){
+//     var a = 
+// b = 100;
+  
+  
+// }
+// raj();
+// // console.log(a)
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
