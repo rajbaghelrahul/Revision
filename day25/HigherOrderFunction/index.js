b6c72048cf0452116ea7dc8be69106a0c19a6fd1
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-14 00:27:39 +0530

    Higher Order Function

--- day25/HigherOrderFunction/index.js
+++ day25/HigherOrderFunction/index.js
@@ -1,3 +1,12 @@
 // console.log("Hello world I'm Higer Order Funciton!!");
 
+const radius = [2, 3, 4, 5];
 
+function calculateAreaOfCircle(rad) {
+    const arr = [];
+    for (let i=0; i<radius.length; i++) {
+        arr.push(Math.PI*rad[i]*rad[i]);
+    }
+    return arr;
+}
+console.log(calculateAreaOfCircle(radius));
