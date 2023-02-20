3505649fa575b714f59e8154be841e80b78eac00
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-21 01:44:21 +0530

    Learning Backend with Dave Gray chapt 3 complete

--- Backend/day29/BackendByDaveGray/chap3/index.js
+++ Backend/day29/BackendByDaveGray/chap3/index.js
@@ -1,2 +1,7 @@
 console.log("I'm Raj Baghel");
 
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'dd-MM-yyyy\tHH:mm:ss'));
+console.log(uuid())
