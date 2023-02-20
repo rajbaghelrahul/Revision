d39c644527c3851ff392c5caad36ba7a7f2c0608
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-21 00:18:52 +0530

    Learning Backend with Dave Gray

--- Backend/day29/BackendByDaveGray/chap2/index2.js
+++ Backend/day29/BackendByDaveGray/chap2/index2.js
@@ -6,7 +6,8 @@ const fileOps = async () => {
     try {
         const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8'); // read file starter.txt
         console.log(data);
-        await fsPromises.writeFile(path.join(__dirname, 'files', 'deletingFile.txt'), data);
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'deletingFile.txt'), data); // create for delete testing purpose.
+        await fsPromises.unlink(path.join(__dirname, 'files', 'deletingFile.txt')); // for deleting command
         await fsPromises.writeFile(path.join(__dirname, 'files', 'newFile.txt'), data); // then write a newFile.
         await fsPromises.appendFile(path.join(__dirname, 'files', 'newFile.txt'), "\n\nToh kese hai aaplog."); // and then append the text as data in same newFile.
         await fsPromises.rename(path.join(__dirname, 'files', 'newFile.txt'), path.join(__dirname, 'files', 'newFilePromiseComplete.txt')); // and then rename the file of newFile to newFilePromiseComplete.
