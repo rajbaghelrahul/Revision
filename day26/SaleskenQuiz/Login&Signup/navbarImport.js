17ef7e766f1e6d0c200e4ea70975bca93ae6162f
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-20 14:44:13 +0530

    Salesken Quiz

---
+++ day26/SaleskenQuiz/Login&Signup/navbarImport.js
@@ -0,0 +1,6 @@
+console.log("Hello world! I'm Report page");
+
+import navbar from "../Navbar/navbar.js";
+
+// document.body.innerHTML = navbar()
+document.getElementById("navbar").innerHTML = navbar();
