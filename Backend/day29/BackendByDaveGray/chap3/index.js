5ae6747117c30a520811ceda504120e3425aad0b
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-21 12:54:53 +0530

    Formate and uuid in chap3

--- Backend/day29/BackendByDaveGray/chap3/index.js
+++ Backend/day29/BackendByDaveGray/chap3/index.js
@@ -3,5 +3,5 @@ console.log("I'm Raj Baghel");
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 
-console.log(format(new Date(), 'dd-MM-yyyy\tHH:mm:ss'));
-console.log(uuid())
+console.log(format(new Date(), 'dd-MM-yyyy\tHH:mm:ss\t'), uuid());
+console.log(uuid());
