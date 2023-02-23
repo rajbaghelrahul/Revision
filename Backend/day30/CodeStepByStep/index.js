9440fc20ffff1879913a40238b5f871af9c8e38f
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-23 14:54:37 +0530

    CodeStepByStep

--- Backend/day30/CodeStepByStep/index.js
+++ Backend/day30/CodeStepByStep/index.js
@@ -1,1 +1,9 @@
 console.log("Hello world!");
+const num = 4%2;
+console.log(num)
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = arr.filter((item) => {
+    return item%2;
+})
+console.log(result);
