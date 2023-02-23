be5f5497a243fc5c6de50f5abbbb77d99dfe3a3f
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-23 21:13:14 +0530

    Create Basic API

--- Backend/day30/CreateBasicAPI/index.js
+++ Backend/day30/CreateBasicAPI/index.js
@@ -1,15 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-    try {
-        res.status(200).send({
-            status: "success",
-            data: "Hello World!"
-        })        
-    } catch (error) {
-        console.log(error);
-    }
-}).listen(3000);
+// http.createServer((req, res) => {
+//     try {
+//         res.status(200).send({
+//             status: "success",
+//             data: "Hello World!"
+//         })        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }).listen(3000);
 
-console.log("Hello World! in Console");
+// console.log("Hello World! in Console");
 
+
+
+const fs = require('fs');
+
+const input = process.argv;
+
+if(input[2] == 'add') {
+    fs.writeFileSync(input[3], input[4]);
+}else if(input[2] == 'remove') {
+    fs.unlinkSync(input[3]);
+}else {
+    console.log("Invalid Input");
+};
