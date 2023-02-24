a2c9376482a04bf8c256c44e5e22220dfa9c88e2
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-25 01:06:29 +0530

    Update Data

--- Backend/day30/cruMongoDB/update.js
+++ Backend/day30/cruMongoDB/update.js
@@ -2,14 +2,22 @@ const connectDB = require("./mongnoDB");
 
 const updateData = async () => {
   let db = await connectDB();
-  let result = await db.updateOne(
+  let result = await db.updateOne( // by "updateOne" if multiple objects by same name or same condition which you are apply then it will update first one only. --> interview question
     { name: 'Raj1 Baghel' }, { 
-        $set: { name: 'Raj1again Baghel' } 
+        $set: { name: 'Raj1again Baghel' } // this is fir updating single thing in single object.
     });
+//   let result = await db.updateOne(
+//     { name: 'Raj1again Baghel' }, { 
+//         $set: { name: 'Raj1again Baghel' , age: 22 } // this is for update many things in single object
+//     });
+//   let result = await db.update( // by "update" if multiple objects by same name or same condition which you are apply in it then it will update all of them.
+//     { name: 'Raj1again Baghel' }, { 
+//         $set: { name: 'Raj1again Baghel' , age: 22 } // this is for update many things in single object
+//     });
     console.log(result);
     if(result.acknowledged) {
         console.log("Data Updated");
     }
 // console.log("Hello World!");
 };
-updateData();
+// updateData();
