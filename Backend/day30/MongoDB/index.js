63182c025dd2e440a9834e747336e87edadbbf9e
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-24 21:44:33 +0530

    MongoDB

--- Backend/day30/MongoDB/index.js
+++ Backend/day30/MongoDB/index.js
@@ -1,1 +1,46 @@
-console.log("Hello world!");
+// const {MongoClient} = require('mongodb');   // we need to install it form npm
+// const url = 'mongodb://localhost:27017';
+// const client = new MongoClient(url);
+
+// const connectDatabase = async () => {
+//     let result = await client.connect();
+//     let db = result.db('revisioin');
+//     let collection = db.collection('first');
+//     let response = await collection.find({}).toArray();
+//     console.log(response);
+// }
+
+// connectDatabase();
+
+
+
+
+
+
+// const {MongoClient} = require('mongodb');
+
+
+// async function connectDB() {
+//     let result = await new MongoClient('mongodb://localhost:27017').connect();
+//     let data = result.db('revisioin');
+//     let collection = data.collection('first');
+//     let res = await collection.find().toArray();
+//     console.log(res);
+// }
+
+// connectDB();
+
+
+
+const {MongoClient} = require('mongodb');
+
+
+async function connectDB() {
+    let result = new MongoClient('mongodb://localhost:27017'); // what is the difference between upper same function line and this line both are working.
+    let data = result.db('revisioin');
+    let collection = data.collection('first');
+    let res = await collection.find().toArray();
+    console.log(res);
+}
+
+connectDB();
