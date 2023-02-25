080f2d574ab14429e3b72c6b5a9d404050b2f5f1
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-25 18:29:05 +0530

    mongoose with API

--- Backend/day30CodeStepByStep/Mongoose/index.js
+++ Backend/day30CodeStepByStep/Mongoose/index.js
@@ -4,7 +4,8 @@ const main = async () => {
     await mongoose.connect('mongodb://localhost:27017/revisioin'); // this is same path of database which is connect with mongodb but automatically next database is created because of method is different.
     const ProdcutSchema = new mongoose.Schema({
         name: String,
-        age: Number
+        age: Number,
+        Occupation: String,
     });
     const ProductModel = mongoose.model('first', ProdcutSchema);
     let data = new ProductModel({name: "Suraj", age: 23, Occupation: "Android Developer"}); // last section "Occupation" will not add in database because of this field is not add in schema.
