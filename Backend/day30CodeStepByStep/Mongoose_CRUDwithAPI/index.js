612f4cc3c5efae43a75ea28dcdaad63a5c28ebf5
rajbaghelrahul <rajbaghelrahul@gmail.com>
2023-02-27 20:36:10 +0530

    Mongoose CRUD operations with API

---
+++ Backend/day30CodeStepByStep/Mongoose_CRUDwithAPI/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+require('./config');
+const Products = require('./products')
+
+const app = express();
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    let result = await Products.find();
+    // let result = await ProductModel.find({name: "Shiva"}); // this for particular Products whose name starting with "Shiva" in DB if we use id instead of it then it will shows single product.
+    console.log(result);
+    res.send("Done!");
+})
+
+app.post('/create', async (req, res) => {
+    let data = new Products(req.body);
+    let result = await data.save();
+    console.log(result);
+    res.send(result);
+})
+
+app.delete('/:id', async (req, res) => {
+    let result = await Products.deleteOne({_id: req.params.id});
+    console.log(result);
+    res.send(result);
+});
+
+app.patch('/', async(req, res) => {
+    let result = await Products.updateOne({name: "Shiva"}, {
+        $set: req.body
+    });
+    console.log(result);
+    res.send(result);
+});
+
+app.listen(3000);
+// console.log("Hello World");
